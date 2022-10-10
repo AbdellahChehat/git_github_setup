1aea0022b598d2d27c06d10f79dd84475d325050
Faduma Muse <faduma@fadumas-mbp.home>
2022-10-10 14:49:09 +0100

    files added to repo

---
+++ main.py
@@ -0,0 +1,44 @@
+# Testing
+# Dynamically typed language
+# Python Variables
+# TO store Any data
+# To store user data - hard code the data - any other type
+# first_name = "Angel" - string
+# DOB = 99 = int
+# uk_resident = yes or No = Boolean
+# travel = 14.5 - float
+# type = find out type of data
+
+#first_name = "Abdellah"
+#last_name = "Chehat"
+
+
+
+#print (first_name)
+
+
+#print("Good morning, Please enter Your Name ")
+
+#name = input()
+#print(name)
+
+#print("Hello dear " + name)
+
+#
+print("Good Morning, Please enter your First Name")
+first_name = input()
+#
+print ("Enter Last Name")
+last_name = input()
+#
+print("Enter DOB")
+DOB = input()
+
+print("Are you a Uk resident? Answer Yes or No")
+
+uk_resident = input()
+
+
+print(f'Hello   {first_name}  {last_name}')
+print (DOB)
+print (uk_resident)
