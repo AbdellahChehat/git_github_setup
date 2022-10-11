226efaf3540f8b7e5eba9def626d7026addccc96
Faduma Muse <faduma@fadumas-mbp.home>
2022-10-11 15:07:12 +0100

    Updated README and added data collection file

---
+++ data_collections.py
@@ -0,0 +1,44 @@
+#Data collections
+# List
+# Tuples
+# Dict
+
+# Lists, synatx ["sdsda" , "sdasd" ]
+
+shopping_list = ["ketchup", "fanta", "apple", "bread"]
+
+# In order to add an item to the list at the end of the list we use ".append"
+shopping_list.append("banana")
+
+# In order to replace an item you need to know the index of the item you are replacing and then add the new item e.g below
+shopping_list [0] = "jam"
+
+# In order to remove items from a list
+shopping_list.remove("fanta")
+
+#removes last item of a list by using pop
+
+shopping_list.pop()
+
+
+print(shopping_list)
+print(type(shopping_list))
+
+
+
+# list can have multiple data types
+
+multi_list = [1,2,3, "string", "apple"]
+
+# Tuples
+# Immutable - can't be (changed - edited - added)
+# Tuples syntax (")
+
+essentials = ("milk", "paracetamol", "drinks")
+print(essentials)
+print(type(essentials))
+
+
+# what is the difference between lists and tuples
+
+
