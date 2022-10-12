4699e53196b438c7b551cf017e4cdf3c3b59af3a
Faduma Muse <faduma@fadumas-mbp.home>
2022-10-12 12:26:48 +0100

    Updated

--- Python Tasks/dic_task.py
+++ Python Tasks/dic_task.py
@@ -7,25 +7,28 @@ story1 = {
     "end":"Unfortunately two wolves spotted him and started to chase him. He run and run and run but it wasn't good enough. The poor little piggy was caught and eaten by the wolves",
 
 }
-##2 - Print the entire dictionary
-
-print(story1)
-
-#3 - Print the type of your dictionary
-
-print(type(story1))
-
-#4 - Print only the keys
-print(story1.keys())
-
-#5 - print only the values
-
-print(story1.values())
-
-#6 - print the individual values using the keys (individually, lots of printi commands)
-print(story1["start"])
-print(story1["middle"])
-print(story1["end"])
+# ##2 - Print the entire dictionary
+#
+# print(story1)
+#
+# #3 - Print the type of your dictionary
+#
+# print(type(story1))
+#
+# #4 - Print only the keys
+# print(story1.keys())
+#
+# #5 - print only the values
+#
+# print(story1.values())
+#
+# #6 - print the individual values using the keys (individually, lots of printi commands)
+# print(story1["start"])
+# print(story1["middle"])
+# print(story1["end"])
 
 #7 - Now let's add a new key:value pair. -  'hero': yourSuperHero
 
+
+story1["hero"] = "Daddy pig"
+print(story1)
